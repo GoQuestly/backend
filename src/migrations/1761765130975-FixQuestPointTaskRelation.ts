fbb2d2fbcc59367b6626d1e95060333991545616
Stanislav Dubina <stanislav.dubina@nure.ua>
2025-10-31 14:55:29 +0200

    chore: fix migration

--- src/migrations/1761765130975-FixQuestPointTaskRelation.ts
+++ src/migrations/1761765130975-FixQuestPointTaskRelation.ts
@@ -4,21 +4,55 @@ export class FixQuestPointTaskRelation1761765130975 implements MigrationInterfac
     name = 'FixQuestPointTaskRelation1761765130975'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "quest_tasks" DROP CONSTRAINT "FK_776a3048badf27ebda112369770"`);
-        await queryRunner.query(`ALTER TABLE "quest_tasks" DROP CONSTRAINT "UQ_776a3048badf27ebda112369770"`);
-        await queryRunner.query(`ALTER TABLE "quest_tasks" DROP COLUMN "quest_point_id"`);
-        await queryRunner.query(`ALTER TABLE "quest_points" ADD "quest_task_id" integer`);
-        await queryRunner.query(`ALTER TABLE "quest_points" ADD CONSTRAINT "UQ_716780a1a0775427ade981f35d4" UNIQUE ("quest_task_id")`);
-        await queryRunner.query(`ALTER TABLE "quest_points" ADD CONSTRAINT "FK_716780a1a0775427ade981f35d4" FOREIGN KEY ("quest_task_id") REFERENCES "quest_tasks"("quest_task_id") ON DELETE SET NULL ON UPDATE NO ACTION`);
+        const questTasksTable = await queryRunner.query(`
+            SELECT column_name 
+            FROM information_schema.columns 
+            WHERE table_name='quest_tasks' AND column_name='quest_point_id'
+        `);
+
+        if (questTasksTable.length > 0) {
+            await queryRunner.query(`
+                ALTER TABLE "quest_tasks" 
+                DROP CONSTRAINT IF EXISTS "FK_776a3048badf27ebda112369770"
+            `);
+
+            await queryRunner.query(`
+                ALTER TABLE "quest_tasks" 
+                DROP CONSTRAINT IF EXISTS "UQ_776a3048badf27ebda112369770"
+            `);
+
+            await queryRunner.query(`
+                ALTER TABLE "quest_tasks" 
+                DROP COLUMN "quest_point_id"
+            `);
+        }
+
+        await queryRunner.query(`
+            ALTER TABLE "quest_points"
+                ADD "quest_task_id" integer
+        `);
+
+        await queryRunner.query(`
+            ALTER TABLE "quest_points"
+                ADD CONSTRAINT "UQ_716780a1a0775427ade981f35d4" UNIQUE ("quest_task_id")
+        `);
+
+        await queryRunner.query(`
+            ALTER TABLE "quest_points"
+                ADD CONSTRAINT "FK_716780a1a0775427ade981f35d4"
+                    FOREIGN KEY ("quest_task_id")
+                        REFERENCES "quest_tasks"("quest_task_id")
+                        ON DELETE SET NULL ON UPDATE NO ACTION
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "quest_points" DROP CONSTRAINT "FK_716780a1a0775427ade981f35d4"`);
         await queryRunner.query(`ALTER TABLE "quest_points" DROP CONSTRAINT "UQ_716780a1a0775427ade981f35d4"`);
         await queryRunner.query(`ALTER TABLE "quest_points" DROP COLUMN "quest_task_id"`);
-        await queryRunner.query(`ALTER TABLE "quest_tasks" ADD "quest_point_id" integer NOT NULL`);
+
+        await queryRunner.query(`ALTER TABLE "quest_tasks" ADD "quest_point_id" integer`);
         await queryRunner.query(`ALTER TABLE "quest_tasks" ADD CONSTRAINT "UQ_776a3048badf27ebda112369770" UNIQUE ("quest_point_id")`);
         await queryRunner.query(`ALTER TABLE "quest_tasks" ADD CONSTRAINT "FK_776a3048badf27ebda112369770" FOREIGN KEY ("quest_point_id") REFERENCES "quest_points"("quest_point_id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
-
 }
